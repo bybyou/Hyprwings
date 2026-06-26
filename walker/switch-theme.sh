9884a8a4690cde314f69e6886fb79a775127e53c
William A. <152408689+bybyou@users.noreply.github.com>
2026-06-26 23:07:10 +0000

    Add cursors

--- walker/switch-theme.sh
+++ walker/switch-theme.sh
@@ -3,6 +3,13 @@
 # Liste des thèmes — ajoute les tiens ici
 themes=("orange" "green" "blue" "pink")
 
+declare -A cursors=(
+    ["orange"]="Adisila Leony"
+    ["green"]="Ceres Fauna"
+    ["blue"]="Hatsune Miku"
+    ["pink"]="Momo Otako"
+)
+
 # Thème actuel
 current=$(grep 'theme = ' ~/.config/walker/config.toml | cut -d'"' -f2)
 
@@ -14,12 +21,17 @@ for i in "${!themes[@]}"; do
     fi
 done
 
-# Passe au suivant (cycle)
+# Passe au suivant
 next_index=$(( (current_index + 1) % ${#themes[@]} ))
 next="${themes[$next_index]}"
 
-# Change le thème
+# Change le thème walker
 sed -i "s/theme = \"$current\"/theme = \"$next\"/" ~/.config/walker/config.toml
 
 # Change le wallpaper
 awww img ~/.config/walker/themes/$next/$next.png --transition-type wipe --transition-angle 45 --transition-duration 1
+
+# Installe le curseur
+sudo cp -r ~/.config/walker/themes/$next/cursor /usr/share/icons/"${cursors[$next]}"
+hyprctl setcursor "${cursors[$next]}" 24
+gsettings set org.gnome.desktop.interface cursor-theme "${cursors[$next]}"
