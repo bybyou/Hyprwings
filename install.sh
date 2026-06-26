24e3a238501c6903a7a14005c801a9b50d14c710
William A. <152408689+bybyou@users.noreply.github.com>
2026-06-26 19:51:45 +0000

    Fix structure

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-sudo pacman -S 
+echo 'alias clr="clear; echo -en \"\e[3J\""' >> ~/.bashrc
+echo 'alias clear="clear; echo -en \"\e[3J\""' >> ~/.bashrc
+sudo pacman -S kitty nemo adw-gtk-theme ttf-jetbrains-mono-nerd make cmake base-devel
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git/
+makepkg -si
+yay -S brave walker elephant elephant-desktopapplications elephant-calc elephant-websearch elephant-providerlist elephant-runner
